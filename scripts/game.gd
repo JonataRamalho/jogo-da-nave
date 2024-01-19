44c0594dd4c1f8f9408aa2d93ca146873bcd94fc
JOÃO VICTOR <jvmv1@aluno.ifal.edu.br>
2024-01-18 21:38:02 -0300

    commit inicial

---
+++ scripts/game.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
