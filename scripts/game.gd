1bf97a2db9921e0dc1e84b9b775bd235bd75f9ca
Jonata Ramalho <jonataramalho.s@gmail.com>
2024-01-20 18:16:14 -0300

    feat: cria player e configura projeto

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var player_spawn_position = $PlayerSpawnPosition
+
+var player = null
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	assert(player!=null)
+	player.global_position = player_spawn_position.position
+	
+
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
