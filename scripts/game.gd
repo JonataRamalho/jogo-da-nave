c9a1a2f08f4a3b6d2c78f9b5e6907b098fd943f8
Jonata Ramalho <jonataramalho.s@gmail.com>
2024-01-20 21:20:35 -0300

    feat: cria tiro, cadência e implementa na nave

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var player_spawn_position = $PlayerSpawnPosition
+@onready var laser_container = $LaserContainer
 
 var player = null
 
@@ -8,6 +9,7 @@ func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	assert(player!=null)
 	player.global_position = player_spawn_position.position
+	player.laser_shot.connect(_on_player_laser_shot)
 	
 
 func _process(delta):
@@ -15,3 +17,8 @@ func _process(delta):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+func _on_player_laser_shot(laser_scene, location):
+	var laser = laser_scene.instantiate()
+	laser.global_position = location
+	laser_container.add_child(laser)
