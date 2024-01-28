6fe0865ce2b3e05f08f642e3c4ee839164e60336
JOÃO VICTOR <jvmv1@aluno.ifal.edu.br>
2024-01-28 13:40:44 -0300

    feat: restringir movimentos dentro da tela; sistema de pontuacao.

--- scripts/player.gd
+++ scripts/player.gd
@@ -24,6 +24,9 @@ func _physics_process(_delta):
 	 "move_right"), Input.get_axis("move_up", "move_down"))
 	velocity = direction * agility
 	move_and_slide()
+	
+	global_position = global_position.clamp(Vector2.ZERO,
+											get_viewport_rect().size)
 
 func shoot():
 	laser_shot.emit(laser_scene, muzzle.global_position)
